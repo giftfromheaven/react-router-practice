64fa35d2a518cebd961c8749634cc43a5a4c6372
Ilya <72022227+giftfromheaven@users.noreply.github.com>
2022-04-05 14:58:00 +0300

    first commit

---
+++ src/pages/NewQuote.js
@@ -0,0 +1,24 @@
+import { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import QuoteForm from '../components/quotes/QuoteForm';
+import useHttp from '../components/hooks/use-http';
+import { addQuote } from '../components/libs/api';
+
+const NewQuote = () => {
+  const { sendRequest, status } = useHttp(addQuote);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (status === 'completed') {
+      history.push('/quotes');
+    }
+  }, [status, history]);
+
+  const addQuoteHandler = (quoteData) => {
+    sendRequest(quoteData);
+    console.log(quoteData);
+  };
+  return <QuoteForm isLoading={status === 'pending'} onAddQuote={addQuoteHandler} />;
+};
+
+export default NewQuote;
